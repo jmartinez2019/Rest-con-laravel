2a14d2d00489dd508695c032348ee1d9824daa1f
jmartinez2019 <jmartinez@existaya.com>
2019-01-08 10:34:04 -0500

    Initial commit

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cliente;
+
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $i = Cliente::all();
+        return $i;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // return con 'vista'
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $cli = new Cliente();
+        $cli->id = $request->id;
+        $cli->nombre = $request->nombre;
+        $cli->apellido = $request->apellido;
+        $cli->email = $request->email;
+        $cli->telefono = $request->telefono;
+        $cli->save();
+
+        return "Se ha almacenado correctamente el usuario ".$cli->nombre;
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+          $cli = Cliente::find($id);
+          return $cli;
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+
+        //return vista
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $cli = Cliente::find($id);
+      $cli->id = $request->id;
+      $cli->nombre = $request->nombre;
+      $cli->apellido = $request->apellido;
+      $cli->email = $request->email;
+      $cli->telefono = $request->telefono;
+      $cli->save();
+
+      return "Se actualizo correctamente el usuario con id: ".$cli->id;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $nit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+
+    {
+          $cli = Cliente::destroy($id);
+          return  "Se borro correctamente";
+    }
+}
---
+++ tests/Unit/ClienteTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ClienteTest extends TestCase
+{
+
+    public function testPruebaPOST()
+  {
+    $r =  $this->json('POST','api/clientes',['id'=>100,'nombre'=>'camilo','apellido'=>'martinez','email'=>'jmartinez@existaya.com','telefono'=>'3206419532']);
+
+    $r->assertStatus(200);
+  }
+
+  public function testPruebaPUT()
+  {
+    $r =  $this->json('PUT','api/clientes/100',['id'=>100,'nombre'=>'camiloModificado','apellido'=>'martinez','email'=>'jmartinez@existaya.com','telefono'=>'3206419532']);
+
+    $r->assertStatus(200);
+  }
+
+  public function testPruebaDELETE()
+  {
+    $r =  $this->json('DELETE','api/clientes/100');
+
+    $r->assertStatus(200);
+  }
+
+  public function testPruebaGET()
+  {
+    $r =  $this->json('GET','api/clientes');
+
+    $r->assertStatus(200);
+  }
+
+  public function testPruebaSHOW()
+  {
+    $r =  $this->json('GET','api/clientes/100');
+
+    $r->assertStatus(200);
+  }
+
+
+
+
+
+
+}
